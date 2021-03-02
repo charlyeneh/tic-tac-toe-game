90e04d1d16fe716300b88cf7d58ddd7d843f7a18
German Renato Cobian <germancobian@hotmail.com>
2021-03-02 11:29:41 -0800

    Create tic tac toe disply board and welcome message

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,13 @@
-#!/usr/bin/env ruby
-puts 'Hello world!'
+# The tic-tac-toe board is displayed
+
+puts "+-----------+"
+puts "| 1 | 2 | 3 |"
+puts "+-----------+"
+puts "| 4 | 5 | 6 |"
+puts "+-----------+"
+puts "| 7 | 8 | 9 |"
+puts "+-----------+"
+
+# A welcome message is displayed so as to inform players that the game is on. Instructions follow.
+puts "Welcome to TIC-TAC-TOE!"
+

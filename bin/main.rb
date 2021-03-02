57813585a309bfc030670a6791380190c9d7e150
Emperor_charly <charleschukwunweikeeneh@gmail.com>
2021-03-02 20:37:14 +0100

    get the individual players names

--- bin/main.rb
+++ bin/main.rb
@@ -10,4 +10,10 @@ puts "+-----------+"
 
 # A welcome message is displayed so as to inform players that the game is on. Instructions follow.
 puts "Welcome to TIC-TAC-TOE!"
-
+# Obtain input from players. This could be their name but it can also be their chosen token
+puts "Player 1 enter your name."
+name_player_1 = gets.chomp
+puts "Player 2 enter your name"
+name_player_2 = gets.chomp
+puts "#{name_player_1}, choose your token: either O or X"
+token_player_1 = gets.strip.upcase

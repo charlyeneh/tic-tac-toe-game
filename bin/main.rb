6d2f52a8d9702c7e7d16449b6af08ddc8a6cdb15
German Renato Cobian <germancobian@hotmail.com>
2021-03-02 11:42:22 -0800

    Create section that checks user input to see that they have chosen one of the correct token options

--- bin/main.rb
+++ bin/main.rb
@@ -17,3 +17,18 @@ puts "Player 2 enter your name"
 name_player_2 = gets.chomp
 puts "#{name_player_1}, choose your token: either O or X"
 token_player_1 = gets.strip.upcase
+
+# Check to see that player 1 chooses either X or O as their token and nothing other
+until %w[X O].include?(token_player_1)
+  puts "That is not a valid token. Choose either O or X"
+  token_player_1 = gets.strip.upcase
+  if token_player_1 != 'X' && token_player_1 != 'O'
+    puts "Ok #{name_player_1}, you get assigned X"
+    token_player_1 = 'X'
+  end
+end
+token_player_2 = token_player_1 == "X" ?
+  "O" : "X"
+puts "#{name_player_1} your token is: #{token_player_1}. #{name_player_2} your token is: #{token_player_2}"
+
+

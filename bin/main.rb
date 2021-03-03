9813b91c93aeb4754070a3aeb94f83ed639fb8e7
Emperor_charly <charleschukwunweikeeneh@gmail.com>
2021-03-03 22:19:10 +0100

    fix lint errors

--- bin/main.rb
+++ bin/main.rb
@@ -22,67 +22,64 @@ until %w[X O].include?(token_player1)
     token_player1 = 'X'
   end
 end
-token_player2 = token_player1 == 'X' ? 'O' : 'X'
-puts "#{name_player1} your token is: #{token_player1}. #{name_player2} your token is: #{token_player2}"
+  token_player2 = token_player1 == 'X' ? 'O' : 'X'
+  puts "#{name_player1} your token is: #{token_player1}. #{name_player2} your token is: #{token_player2}"
 
 no_further = false
 until no_further
   board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   i = 0
 
-  # Run each instance of the game until either there is a winner or all positions are taken
-  while i < 9
-    if i.even?
-      (player = name_player1)
-      token = token_player1
-    else
-      (player = name_player2)
-      token = token_player2
-    end
-    puts "#{player} its your move. Enter the number of   the position where you wish to place your token."
-    move = gets.chomp.to_i - 1
+# Run each instance of the game until either there is a winner or all positions are taken
+while i < 9
+  if i.even?
+    (player = name_player1)
+    token = token_player1
+  else
+    (player = name_player2)
+    token = token_player2
+  end
+  puts "#{player} its your move. Enter the number of  the position where you wish to place your token."
+  move = gets.chomp.to_i - 1
 
-    # The updated tic-tac-toe board is displayed with the   tokens displayed on their assigned positions.
-    p board
+# The updated tic-tac-toe board is displayed with the   tokens displayed on their assigned positions.
+p board
 
-    # Deciding the game outcome
-  game_on = true
-  winner = false
-  draw = false
-  # initial instructions for player
-
-  while game_on
+# Deciding the game outcome
+game_on = true
+winner = false
+draw = false
+  
+# initial instructions for player
+while game_on
   puts 'Player_1 your turn. Choose a number between 1 and 9'
-  # loop for each move
-  movement = false
-  while movement == false
-    p_one = gets.chomp.to_i
-    if (1..9).include?(p_one)
-      movement = true
-      board
-    else
-      puts 'wrong input'
-    end
+# loop for each move
+movement = false
+while movement == false
+  p_one = gets.chomp.to_i
+  if (1..9).include?(p_one)
+    movement = true
+    board
+  else
+    puts 'wrong input'
   end
+end
 
-  game_on = false if winner || draw
-
-  # loop for each move
-  puts 'Player2 your turn. Choose a number between 1 and 9'
-  movement = false
-  while movement == false
-    p_two = gets.chomp.to_i
-    if (1..9).include?(p_two)
-      movement = true
-      board
-      winner = player_one.to_s
-    else
-      puts 'wrong input'
-    end
+game_on = false if winner || draw
+# loop for each move
+puts 'Player2 your turn. Choose a number between 1 and9'
+movement = false
+while movement == false
+  p_two = gets.chomp.to_i
+  if (1..9).include?(p_two)
+    movement = true
+    board
+    winner = player_one.to_s
+  else
+    puts 'wrong input'
   end
-
+end
   game_on = false if winner || draw
-
 end
 
 # display the result after winning
@@ -92,11 +89,10 @@ puts "The winner is #{winner}" if winning
 #  draww movement
 puts 'There is no winner, It is a DRAW' if draw 
 
-  # Players are asked if they'd like to play another round or end the game. The game ends or re-starts accordingly.
-  puts 'Do you wish to play another round? Y / N'
-  willing = gets.chomp
-  if willing == 'N'
-    no_further = true
-    puts 'Game over!'
-  end
+# Players are asked if they'd like to play another round or end the game. The game ends or re-starts accordingly.
+puts 'Do you wish to play another round? Y / N'
+willing = gets.chomp
+if willing == 'N'
+  no_further = true
+  puts 'Game over!'
 end

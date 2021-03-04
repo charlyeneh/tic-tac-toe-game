9f6b05ffa0d245b0dedfce257207d88bc6d7df83
German Renato Cobian <germancobian@hotmail.com>
2021-03-04 15:39:20 -0800

    Correct issues pointed out by code reviewers

--- lib/board.rb
+++ lib/board.rb
@@ -8,11 +8,12 @@ class Board
   end
 
   def display
-    puts "   #{@board[0]}   |    #{@board[1]}    |    #{@board[2]}   "
-    puts '---------------------------'
-    puts "  #{@board[3]}    |    #{@board[4]}    |    #{@board[5]}   "
-    puts '---------------------------'
-    puts "  #{@board[6]}    |    #{@board[7]}    |    #{@board[8]}   "
+    %(  
+         #{@board[0]}   |    #{@board[1]}    |    #{@board[2]}
+      ---------------------------
+        #{@board[3]}    |    #{@board[4]}    |    #{@board[5]}
+      ---------------------------
+        #{@board[6]}    |    #{@board[7]}    |    #{@board[8]})
   end
 
   def update_board(location, token)

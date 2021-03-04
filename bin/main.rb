5bed9713691f0d9ad04e9ba4192fe5ac7c1cbe77
German Renato Cobian <germancobian@hotmail.com>
2021-03-04 07:10:35 -0800

    Create class Player for the storing of game player variables

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,30 @@
-#!/usr/bin/env ruby
-puts 'Hello world!'
+class Player
+  attr_reader :name, :token
+  def initialize(name, token)
+    @name = name
+    @token = token
+  end
+end
+
+
+puts 'Welcome to TIC-TAC-TOE!'
+puts 'Player 1 enter your name.'
+name_player1 = gets.chomp.upcase
+puts 'Player 2 enter your name'
+name_player2 = gets.chomp.upcase
+puts "#{name_player1}, choose your token: either O or X"
+token_player1 = gets.strip.upcase
+until %w[X O].include?(token_player1)
+  puts 'That is not a valid token. Choose either O or X'
+  token_player1 = gets.strip.upcase
+  if token_player1 != 'X' && token_player1 != 'O'
+    puts "Ok #{name_player1}, you get assigned X"
+    token_player1 = 'X'
+  end
+end
+token_player2 = token_player1 == 'X' ? 'O' : 'X'
+
+player1 = Player.new(name_player1, token_player1)
+player2 = Player.new(name_player2, token_player2)
+
+

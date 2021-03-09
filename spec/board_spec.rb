15520797f59a4b060e1283045a79bead287af7b2
Mihndim <jelsieprisca2016@gmail.com>
2021-03-09 14:29:18 -0500

    Add tests to win_check and Fix linter error

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,7 +6,7 @@ describe Board do
     it 'Prints board grid' do
       board = Board.new
       @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-      expect(board.display).to eq(  %(
+      expect(board.display).to eq(%(
         #{@board[0]}    |    #{@board[1]}    |    #{@board[2]}
       ---------------------------
         #{@board[3]}    |    #{@board[4]}    |    #{@board[5]}
@@ -17,10 +17,10 @@ describe Board do
 end
 
 describe '#update_board' do
-    it 'updates chosen position to player assigned token' do
-      board = Board.new
-      expect(board.update_board(4, 'X')).to eql('X')
-    end
+  it 'updates chosen position to player assigned token' do
+    board = Board.new
+    expect(board.update_board(4, 'X')).to eql('X')
+  end
 end
 
 describe '#position_taken?' do
@@ -39,19 +39,85 @@ describe '#position_taken?' do
 end
 
 describe '#draw?' do
-    it 'returns true if all elements in board are a string' do
-      board = Board.new
-      player = Player.new('German', 'O', board)
+  it 'returns true if all elements in board are a string' do
+    board = Board.new
+    player = Player.new('German', 'O', board)
 
-      9.times { |index| player.turn(index + 1) }
-      expect(board.draw?).to be(true)
-    end
-
-    it 'returns false if not all elements in board are a string' do
-      board = Board.new
-      player = Player.new('German', 'O', board)
-
-      8.times { |index| player.turn(index + 1) }
-      expect(board.draw?).to be(false)
-    end
+    9.times { |index| player.turn(index) }
+    expect(board.draw?).to be(true)
   end
+
+  it 'returns false if not all elements in board are a string' do
+    board = Board.new
+    player = Player.new('German', 'O', board)
+
+    8.times { |index| player.turn(index) }
+    expect(board.draw?).to be(false)
+  end
+end
+
+describe '#win_check?' do
+  it 'returns true if token has three matches horizontally' do
+    board = Board.new
+    player = Player.new('German', 'X', board)
+    location = [0, 1, 2]
+    location.each { |val| player.turn(val) }
+    expect(board.win_check?).to be(true)
+  end
+  it 'returns true if token has three matches horizontally' do
+    board = Board.new
+    player = Player.new('Julius', 'O', board)
+    location = [3, 4, 5]
+    location.each { |val| player.turn(val) }
+    expect(board.win_check?).to be(true)
+  end
+  it 'returns true if token has three matches horizontally' do
+    board = Board.new
+    player = Player.new('German', 'X', board)
+    location = [6, 7, 8]
+    location.each { |val| player.turn(val) }
+    expect(board.win_check?).to be(true)
+  end
+  it 'returns true if token has three matches horizontally' do
+    board = Board.new
+    player = Player.new('Julius', 'O', board)
+    location = [0, 3, 6]
+    location.each { |val| player.turn(val) }
+    expect(board.win_check?).to be(true)
+  end
+  it 'returns true if token has three matches horizontally' do
+    board = Board.new
+    player = Player.new('German', 'X', board)
+    location = [1, 4, 7]
+    location.each { |val| player.turn(val) }
+    expect(board.win_check?).to be(true)
+  end
+  it 'returns true if token has three matches horizontally' do
+    board = Board.new
+    player = Player.new('Julius', 'O', board)
+    location = [2, 5, 8]
+    location.each { |val| player.turn(val) }
+    expect(board.win_check?).to be(true)
+  end
+  it 'returns true if token has three matches horizontally' do
+    board = Board.new
+    player = Player.new('German', 'X', board)
+    location = [0, 4, 8]
+    location.each { |val| player.turn(val) }
+    expect(board.win_check?).to be(true)
+  end
+  it 'returns true if token has three matches horizontally' do
+    board = Board.new
+    player = Player.new('Julius', 'O', board)
+    location = [2, 4, 6]
+    location.each { |val| player.turn(val) }
+    expect(board.win_check?).to be(true)
+  end
+  it 'returns false if tokens do not line up either horizontally, vertically, or diagonally' do
+    board = Board.new
+    player = Player.new('German', 'X', board)
+    location = [0, 5, 7]
+    location.each { |val| player.turn(val) }
+    expect(board.win_check?).to be(false)
+  end
+end
